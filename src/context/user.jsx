29d0a7f6a9b7964d79dd702526d8748e49819c22
Wanambuko Ngoya Daniel <danielngoys@outlook.com>
2022-12-27 01:26:35 +0300

    ABC lms

---
+++ src/context/user.jsx
@@ -0,0 +1,33 @@
+import React, { useState, useEffect} from "react";
+
+const UserContext = React.createContext();
+
+function UserProvider({ children }) {
+    const [user, setUser] = useState(null);
+
+    useEffect(() => {
+        fetch("https://lms-4etp.onrender.com/authorized_user").then(
+          (response) => {
+            if (response.ok) {
+              response.json().then((user) => {
+                setUser(user);
+              });
+            } else {
+              response.json().then((json) => {
+                if (json.errors !== undefined) {
+                  alert(json.errors);
+                }
+              });
+            }
+          }
+        );
+      },[]);
+
+    return (
+      <UserContext.Provider value={{ user, setUser }}>
+        {children}
+      </UserContext.Provider>
+    );
+  }
+
+export { UserContext, UserProvider };
